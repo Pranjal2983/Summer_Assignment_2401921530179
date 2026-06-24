030b1e1eca6a42e6ae40e9657a9fb05a6ab07a7c
Pranjal Gautam <pranjalgautam7788@gmail.com>
2026-06-24 20:06:34 +0530

    Create Inorder.java

---
+++ Week_4/Day_2/Inorder.java
@@ -0,0 +1,15 @@
+ import java.util.ArrayList;
+import java.util.List;
+class Solution {
+    ArrayList<Integer> list = new ArrayList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if(root == null){
+            return list ;
+        }
+        inorderTraversal(root.left);
+            list.add(root.val);
+            inorderTraversal(root.right);
+             return list;
+
+    }
+}
